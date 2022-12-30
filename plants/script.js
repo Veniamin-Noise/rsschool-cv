fcab882af08c04c63bb78ad33a7b98fb0b1b4acd
Veniamin <112535019+Veniamin-Noise@users.noreply.github.com>
2022-12-30 23:53:11 +0600

    add: task plants

---
+++ plants/script.js
@@ -0,0 +1,31 @@
+// let acc = document.getElementsByClassName("accordion");
+// let i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     let panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     }
+//   });
+// }
+
+// function showDiv(opposites) {
+//     opposites.forEach(function(opposite) {
+//         if (document.getElementById(opposite).style.display === "block") document.getElementById(opposite).style.display = "none";
+//         else document.getElementById(opposite).style.display = "block";
+//     });
+// }
+
+// function showDiv(opens, closes) {
+//     opens.forEach(function(open) {
+//         document.getElementById(open).style.display = "block";
+//     });
+
+//     closes.forEach(function(close) {
+//         document.getElementById(close).style.display = "none";
+//     });
+// }
